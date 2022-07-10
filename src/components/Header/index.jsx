4be155aa92027616e0b050127e3762c52b76172b
nilsmanc <etereaiberia@gmail.com>
2022-07-10 14:50:07 +0300

    auth

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -15,7 +15,7 @@ export const Header = () => {
       <Container maxWidth="lg">
         <div className={styles.inner}>
           <Link className={styles.logo} to="/">
-            <div>TRAINING BLOG</div>
+            <div>BLOG</div>
           </Link>
           <div className={styles.buttons}>
             {isAuth ? (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,17 +6,19 @@ import Grid from '@mui/material/Grid';
 import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
 import { CommentsBlock } from '../components/CommentsBlock';
-import { fetchPosts } from '../redux/slices/posts';
+import { fetchPosts, fetchTags } from '../redux/slices/posts';
 
 export const Home = () => {
   const dispatch = useDispatch();
   const { posts, tags } = useSelector((state) => state.posts);
 
   const isPostsLoading = posts.status == 'loading';
+  const isTagsLoading = tags.status == 'loading';
   React.useEffect(() => {
     dispatch(fetchPosts());
+    dispatch(fetchTags());
   }, []);
-
+  debugger;
   return (
     <>
       <Tabs style={{ marginBottom: 15 }} value={0} aria-label="basic tabs example">
@@ -32,8 +34,11 @@ export const Home = () => {
               <Post
                 id={obj._id}
                 title={obj.title}
-                imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-                user={obj.user}
+                imageUrl={obj.imageUrl}
+                user={{
+                  avatarUrl: obj.avatarUrl,
+                  fullName: obj.fullName,
+                }}
                 createdAt={obj.createdAt}
                 viewsCount={obj.viewsCount}
                 commentsCount={3}
@@ -44,7 +49,7 @@ export const Home = () => {
           )}
         </Grid>
         <Grid xs={4} item>
-          <TagsBlock items={['react', 'typescript', 'заметки']} isLoading={false} />
+          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
           <CommentsBlock
             items={[
               {
